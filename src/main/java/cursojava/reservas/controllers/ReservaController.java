3168bd4268d4bdb90d4bc34c48c6fba9a3e36d71
Jairo Chapela-Martínez <jairochapela@gmail.com>
2026-03-12 19:14:10 +0100

    Agregar funcionalidades para mostrar, editar y eliminar reservas.

--- src/main/java/cursojava/reservas/controllers/ReservaController.java
+++ src/main/java/cursojava/reservas/controllers/ReservaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import cursojava.reservas.models.Habitacion;
@@ -43,5 +44,40 @@ public class ReservaController {
         reserva.setLocalizador(UUID.randomUUID());
         reservaRepository.save(reserva);
         return "redirect:/reservas";
+    }
+
+    @GetMapping("/reservas/{localizador}")
+    public String mostrarDetalleReserva(@PathVariable UUID localizador, Model modelUI) {
+        Reserva reserva = reservaRepository.findById(localizador).orElse(null);
+        if (reserva == null) {
+            return "redirect:/reservas"; // TODO: mejor mostrar un mensaje de error o una página de "Reserva no encontrada"
+        }
+        modelUI.addAttribute("reserva", reserva);
+        return "detallereserva";
+    }
+
+    @PostMapping("/reservas/{localizador}/eliminar")
+    public String eliminarReserva(@PathVariable UUID localizador) {
+        reservaRepository.deleteById(localizador);
+        return "redirect:/reservas";
+    }
+
+    @GetMapping("/reservas/{localizador}/editar")
+    public String mostrarFormularioEditarReserva(@PathVariable UUID localizador, Model modelUI) {
+        Reserva reserva = reservaRepository.findById(localizador).orElse(null);
+        if (reserva == null) {
+            return "redirect:/reservas"; // TODO: mejor mostrar un mensaje de error o una página de "Reserva no encontrada"
+        }
+        List<Habitacion> habitaciones = habitacionRepository.findAll();
+        modelUI.addAttribute("reserva", reserva);
+        modelUI.addAttribute("habitaciones", habitaciones);
+        return "editarreserva";
+    }
+
+    @PostMapping("/reservas/{localizador}")
+    public String actualizarReserva(@PathVariable UUID localizador, Reserva reservaActualizada) {
+        reservaActualizada.setLocalizador(localizador);
+        reservaRepository.save(reservaActualizada);
+        return "redirect:/reservas/" + localizador;
     }
 }
